ddf3315ee4b7668dfc5f79e83e3a55375e4d514e
mesidex <mesidexpimppi@gmail.com>
2026-06-08 23:30:39 +0300

    Delete nilinstance-dump.lua

--- nilinstance-dump.lua
+++
@@ -1,8 +0,0 @@
-for _, inst in ipairs(getnilinstances()) do
-    if inst:IsA("LocalScript") or inst:IsA("ModuleScript") then
-        local src = decompile(inst)
-        if src and #src > 0 then
-            writefile(inst.Name .. ".lua", src)
-        end
-    end
-end
